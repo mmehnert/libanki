8f51bb58c9e4d6365bf3533ed5fd2cab92b7e09a
Maximilian Mehnert <maximilian.mehnert@gmx.de>
2010-12-13 04:19:32 +0100

    If latex image cannot be generated, display the latex source code.

--- anki/latex.py
+++ anki/latex.py
@@ -121,6 +121,8 @@ def imageForLatex(deck, latex, build=True):
     ok = True
     if build and (not imageFile or not os.path.exists(imageFile)):
         (ok, imageFile) = buildImg(deck, latex)
+    if not ok or not os.path.exists(imageFile):
+        return (False, latex)
     if not ok:
         return (False, imageFile)
     return (True, imageFile)
@@ -132,7 +134,7 @@ def imgLink(deck, latex, build=True):
     if ok:
         return '<img src="%s" alt="%s">' % (img, latex)
     else:
-        return img
+        return latex
 
 def formatQA(html, type, cid, mid, fact, tags, cm, deck):
     return renderLatex(deck, html)
